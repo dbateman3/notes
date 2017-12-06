177a676343eb1170163b6dacbe2ee3c38832a554
Dan Bateman <dbateman0319@starkstate.net>
2017-12-05 21:28:30 -0500

    Console argument functions created

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const yargs = require('yargs');
 const argv = yargs.argv;
 let command = process.argv[2];
 console.log(`Command: ${command}`);
-console.log('Process', process.argv);
 console.log('Yarg', argv);
 
 if (command === 'add') {
@@ -19,9 +18,9 @@ if (command === 'add') {
 } else if (command === 'list') {
 	notes.getAll();
 } else if (command === 'read') {
-	console.log('Reading note');
+	notes.getNote(argv.title);
 } else if (command === 'remove') {
-	console.log('Removing note');
+	notes.removeNote(argv.title);
 } else {
 	console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -4,6 +4,21 @@ let addNote = function(title, body) {
 	console.log('Adding note', title, body)
 };
 
+let getAll = function() {
+	console.log(`Getting notes`);
+};
+
+let getNote = function(title) {
+	console.log(`Getting ${title} note`);
+};
+
+let removeNote = function(title) {
+	console.log(`Removing ${title} note`);
+};
+
 module.exports = {
-	addNote: addNote
+	addNote: addNote,
+	getAll: getAll,
+	getNote: getNote,
+	removeNote: removeNote
 };
